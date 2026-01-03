4f308205f00eaa97490707de75ad69f58c283039
Justin Ovens <jovens@gotunix.net>
2026-01-03 18:33:49 -0500

    Pushing pip template

---
+++ src/pip_template/__init__.py
@@ -0,0 +1,6 @@
+import importlib.metadata
+
+PROJECT = "pip_template"
+VERSION = importlib.metadata.version(PROJECT)
+
+__version__ = VERSION
---
+++ src/pip_template/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from pip_template import __version__ as VERSION
+
+def main():
+    print(f"Version: {VERSION}")
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
